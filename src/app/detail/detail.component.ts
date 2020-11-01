9055f5cf650f1473d5babc4f5e641f7bbf312c68
Partha <partha@localhost.localdomain>
2020-11-01 06:48:16 -0500

    update

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -13,6 +13,8 @@ export class DetailComponent implements OnInit {
   data$: any;
   public IMAGE_BASE_PATH: string = "";
   public movieDetail: any;
+  public release_date: string;
+  public release_year: string;
 
   constructor(public router: Router, public route: ActivatedRoute,
     private detailservice: DetailService) {
@@ -29,7 +31,46 @@ export class DetailComponent implements OnInit {
       .subscribe((response: any) => {
         this.movieDetail = response;
         console.log(this.movieDetail);
+        let release_date = this.movieDetail.release_date;
+        this.release_year = release_date.split('-')[0];
+        this.monthConvert(this.movieDetail.release_date);
       });
+  }
+
+  private monthConvert(dates){
+    let dt = parseInt(dates.split('-')[1]);
+    let yr = dates.split('-')[0];
+    let dat = dates.split('-')[2];
+    let name = '';
+    if(dt === 0){
+      name = 'Jan';
+    }else if(dt === 1){
+      name = 'Jan';
+    }else if(dt === 2){
+      name = 'Jan';
+    }else if(dt === 3){
+      name = 'Feb';
+    }else if(dt === 4){
+      name = 'Mar';
+    }else if(dt === 5){
+      name = 'Apr';
+    }else if(dt === 6){
+      name = 'Jun';
+    }else if(dt === 7){
+      name = 'Jul';
+    }else if(dt === 8){
+      name = 'Aug';
+    }else if(dt === 9){
+      name = 'Sep';
+    }else if(dt === 10){
+      name = 'Oct';
+    }else if(dt === 11){
+      name = 'Nov';
+    }else if(dt === 12){
+      name = 'Dec';
+    }
+    let final = name + ' ' + dat + ',' + yr;
+    this.release_date = final;
   }
 
 }
